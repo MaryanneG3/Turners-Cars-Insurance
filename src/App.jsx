d46f4852572c0bdb423377361a4565430f109bb4
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-17 00:20:47 +1300

    corrected Homepage component link in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/homepage/Homepage";
+import Homepage from "./pages/cars/homepage/Homepage";
 
 function App() {
   return (
