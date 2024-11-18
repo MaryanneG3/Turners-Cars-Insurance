fc90ed32e0ff1cf7b897c9797a1079d9333b0376
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-18 22:50:25 +1300

    added VehicleSidebar component and implemented in the NavbarNavlinks page

---
+++ src/common/vehicleSidebar/VehicleSidebar.jsx
@@ -0,0 +1,40 @@
+// const links = [
+//   { to: "#Cars-page", label: "Cars" },
+//   { to: "#Trucks & Machinery", label: "Trucks & Machinery" },
+//   { to: "#Damaged & End of Life", label: "Damaged & End of Life" },
+//   { to: "#Motorcycles", label: "Motorcycles" },
+//   { to: "#General Goods", label: "General Goods" },
+//   {
+//     to: "#Buses, Caravans & Motorhomes",
+//     label: "Buses, Caravans & Motorhomes",
+//   },
+//   { to: "#Boats & Marine", label: "Boats & Marine" },
+// ];
+
+function VehicleSidebar() {
+  return <div>Testing</div>;
+}
+
+export default VehicleSidebar;
+
+{
+  /* <DropdownMenu
+  menu="vehicles"
+  title="Vehicles"
+  icon={faAngleDoubleDown}
+  dropdowns={dropdowns}
+  toggleMenu={toggleMenu}
+  links={[
+    { to: "#Cars-page", label: "Cars" },
+    { to: "#Trucks & Machinery", label: "Trucks & Machinery" },
+    { to: "#Damaged & End of Life", label: "Damaged & End of Life" },
+    { to: "#Motorcycles", label: "Motorcycles" },
+    { to: "#General Goods", label: "General Goods" },
+    {
+      to: "#Buses, Caravans & Motorhomes",
+      label: "Buses, Caravans & Motorhomes",
+    },
+    { to: "#Boats & Marine", label: "Boats & Marine" },
+  ]}
+/>; */
+}
