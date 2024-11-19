79a83ed142208d8143418fa87f940ad269b94f53
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-19 17:12:36 +1300

    fixed spelling errors and tidied up code

--- src/components/navbarNavlinks/NavbarNavlinks.jsx
+++ src/components/navbarNavlinks/NavbarNavlinks.jsx
@@ -5,19 +5,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faAngleDown,
-  faAngleDoubleDown,
 } from "@fortawesome/free-solid-svg-icons";
 import DropdownMenu from "./DropdownMenu";
 import VehicleSidebar from "../../common/vehicleSidebar/VehicleSidebar";
 
-const NavbarNavlinks = () => {
+function NavbarNavlinks() {
   const [dropdowns, setDropdowns] = useState({
     howToBuy: false,
     sellYourCar: false,
     financeAndInsurance: false,
     auctions: false,
     servicesAndInfo: false,
-    vehicles: false,
   });
 
   const toggleMenu = (menu) => {
@@ -36,19 +34,19 @@ const NavbarNavlinks = () => {
         </NavLink>
 
         <DropdownMenu
-          menu="hotToBuy"
+          menu="howToBuy"
           title="How to Buy"
           icon={faAngleDown}
           dropdowns={dropdowns}
           toggleMenu={toggleMenu}
           links={[
-            { to: "#opt 1", label: "View our Cars" },
-            { to: "#opt 2", label: "Ways to Buy" },
-            { to: "#opt 3", label: "Book a test drive" },
-            { to: "#opt 4", label: "How to pay" },
-            { to: "#opt 5", label: "FAQs about buying" },
-            { to: "#opt 6", label: "Turners live" },
-            { to: "#opt 7", label: "Website tools" },
+            { to: "#view-our-cars-page", label: "View our Cars" },
+            { to: "#ways-to-buy", label: "Ways to Buy" },
+            { to: "#book-a-test-drive", label: "Book a test drive" },
+            { to: "#how-to-pay", label: "How to pay" },
+            { to: "#faqs-about-buying", label: "FAQs about buying" },
+            { to: "#turners-live", label: "Turners live" },
+            { to: "#website-tools", label: "Website tools" },
           ]}
         />
 
@@ -59,9 +57,9 @@ const NavbarNavlinks = () => {
           dropdowns={dropdowns}
           toggleMenu={toggleMenu}
           links={[
-            { to: "#opt 1", label: "Ways to sell" },
-            { to: "#opt 2", label: "Book an appraisal" },
-            { to: "#opt 3", label: "FAQs about selling" },
+            { to: "#ways-to-sell", label: "Ways to sell" },
+            { to: "#book-an-appraisal", label: "Book an appraisal" },
+            { to: "#faqs-about-selling", label: "FAQs about selling" },
           ]}
         />
 
@@ -72,11 +70,17 @@ const NavbarNavlinks = () => {
           dropdowns={dropdowns}
           toggleMenu={toggleMenu}
           links={[
-            { to: "#opt 1", label: "Finance information" },
-            { to: "#opt 2", label: "Finance FAQ" },
-            { to: "#opt 3", label: "Apply Online for Finance" },
-            { to: "#opt 4", label: "Apply Online for Business Finance" },
-            { to: "#opt 5", label: "Car Insurance" },
+            { to: "#finance-info", label: "Finance information" },
+            { to: "#finance-faq", label: "Finance FAQ" },
+            {
+              to: "#apply-online-for-finance",
+              label: "Apply Online for Finance",
+            },
+            {
+              to: "#apply-for-business-finance",
+              label: "Apply Online for Business Finance",
+            },
+            { to: "#car-insurance", label: "Car Insurance" },
           ]}
         />
       </div>
@@ -89,11 +93,11 @@ const NavbarNavlinks = () => {
           dropdowns={dropdowns}
           toggleMenu={toggleMenu}
           links={[
-            { to: "#opt 1", label: "All car auctions" },
-            { to: "#opt 2", label: "Whangarei" },
-            { to: "#opt 3", label: "North West Auckland" },
-            { to: "#opt 4", label: "Hamilton" },
-            { to: "#opt 5", label: "Tauranga" },
+            { to: "#all-car-auctions", label: "All car auctions" },
+            { to: "#whangarei", label: "Whangarei" },
+            { to: "#nw-auckland-page", label: "North West Auckland" },
+            { to: "#hamilton", label: "Hamilton" },
+            { to: "#tauranga", label: "Tauranga" },
           ]}
         />
 
@@ -104,9 +108,9 @@ const NavbarNavlinks = () => {
           dropdowns={dropdowns}
           toggleMenu={toggleMenu}
           links={[
-            { to: "#opt 1", label: "5 day Money Back Guarentee" },
-            { to: "#opt 2", label: "Turners Group Overview" },
-            { to: "#opt 3", label: "Terms & Conditions" },
+            { to: "#money-back-page", label: "5 day Money Back Guarentee" },
+            { to: "#turners-overview", label: "Turners Group Overview" },
+            { to: "#terms-and-conditions", label: "Terms & Conditions" },
           ]}
         />
 
@@ -114,6 +118,6 @@ const NavbarNavlinks = () => {
       </div>
     </div>
   );
-};
+}
 
 export default NavbarNavlinks;
