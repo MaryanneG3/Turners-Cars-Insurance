514d5226816a331307fc389780943ac226ce24c2
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-21 02:20:22 +1300

    added placeholder img for main content, created Insurance Quote page

---
+++ src/pages/cars/insurancepage/InsuranceQuote.jsx
@@ -0,0 +1,12 @@
+import BaseLayout from "../../../layouts/baselayout/BaseLayout";
+import styles from "./InsuranceQuote.module.css";
+
+function InsuranceQuote() {
+  return (
+    <BaseLayout>
+      <div className={styles.mainContentContainer}></div>
+    </BaseLayout>
+  );
+}
+
+export default InsuranceQuote;
