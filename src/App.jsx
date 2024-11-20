be6653152a16b467e823c246a70b48a4585ccaf4
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-21 02:20:51 +1300

    added route to insurance quote page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/cars/homepage/Homepage";
+import InsuranceQuote from "./pages/cars/insurancepage/InsuranceQuote";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/get-a-quote-page" element={<InsuranceQuote />} />
     </Routes>
   );
 }
